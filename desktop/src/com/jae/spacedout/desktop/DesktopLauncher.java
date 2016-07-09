98834e971a5b64121d1af1b14f0170cd8158ff14
Anton <aopanis@gmail.com>
2016-07-09 13:36:52 -0700

    Made screen dimensions static variables in Setting class

--- desktop/src/com/jae/spacedout/desktop/DesktopLauncher.java
+++ desktop/src/com/jae/spacedout/desktop/DesktopLauncher.java
@@ -3,14 +3,15 @@ package com.jae.spacedout.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jae.spacedout.SpacedOut;
+import com.jae.spacedout.utility.Settings;
 
 public class DesktopLauncher
 {
     public static void main(String[] arg)
     {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 960;
-        config.height = 540;
+        config.width = Settings.GAME_SCREEN_WIDTH;
+        config.height = Settings.GAME_SCREEN_HEIGHT;
         config.resizable = true;
         new LwjglApplication(new SpacedOut(), config);
     }
